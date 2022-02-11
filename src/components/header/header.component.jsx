a0f4945960c9788f61c648bb3cc1fd9ed7438946
jowelx <jzambranojazc2001@gmail.com>
2022-02-11 15:42:49 -0400

    arrow-Menu

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import HeaderDropDown from "../header-dropdown/header-drop-down.component";
 import Person from "../../assets/images/person.jpg";
 import { Link } from "react-router-dom";
-
+import ArrowMenu from "../arrow-menu/arrow.menu";
 import "./header.styles.scss";
 import { useSelector } from "react-redux";
 const Header = () => {
@@ -36,7 +36,9 @@ const Header = () => {
             <img src={Person} alt="" />
           </div>
           <div className="user-name">{user?.user?.name}</div>
-          <span class="material-icons">expand_more</span>
+           <div >
+           <ArrowMenu/>
+           </div>
         </div>
         {dropActive && <HeaderDropDown />}
       </div>
--- src/pages/notification/notification.pages.jsx
+++ src/pages/notification/notification.pages.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
-
+import "./notification.styles.css"
 const notification=()=> {
   return (
     <div className="dashboard-page">
          <div className="dashboard-page-heading">
         <h2>Notifications</h2>
-      </div>   
+      </div>
+    <div className="notification-container">
+  
+   
+    <div className="notification-card">
+      <div>
+
+      <h6 className="notification-tittle">
+        Milestone accepted
+        </h6>
+        <p className="notification-date">
+          10 fev,2022
+        </p>
+      </div>
+     
+        <span class="material-icons">cancel</span>
+    </div>
+    </div>
     </div>
   )
 }
